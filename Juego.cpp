067b6a8b76199f3b1a5a76a280a41246bb4db0f2
Alberto00093924 <00093924@uca.edu.sv>
2024-07-07 15:24:54 -0600

    declarando variable

---
+++ Juego.cpp
@@ -0,0 +1,9 @@
+#include <angelf25.hpp>
+
+// Constantes
+const int MAX_INTENTOS = 6;
+
+// Funciones
+void dibujarAhorcado(int intentos);
+bool adivinarLetra(char letra, string& palabra, vector<char>& letrasAdivinadas);
+void jugar();
