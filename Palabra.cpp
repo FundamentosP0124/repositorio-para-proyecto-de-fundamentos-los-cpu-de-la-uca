5665c12ff3f759bb541a48b26c515b600f3accac
astrxss <00225424@uca.edu.sv>
2024-07-07 20:42:47 -0600

    Correcion de comentario y cambio del nombre de funcion para lvl medio

--- Palabra.cpp
+++ Palabra.cpp
@@ -1,21 +1,23 @@
 #include <ideamen.hpp>
 
-//cambio del nombre de la funcion 
+//Funcion para seleccionar la palabras para el nivel facil.
 string palabralvlfacil() {
-    string letra[10] = {"mano", "casa", "flor", "azul", "lobo", "cama", "luna", "relo", "vela", "cima"};
-    int a;
+    string letra[10] = {"mano", "casa", "flor", "azul", "lobo", "cama", "luna", "relo", "vela", "cima"}; //arreglo de 10 palabras.
+
+    int a; 
     time_t t;
-    t = time(NULL); //desde (int a;) hasta (a = rand), se encargan de selecionar un numero aleatorio del 1 al 1o
+    t = time(NULL); 
     srand(t);
-    a = rand() % 10; 
+    a = rand() % 10; //seleccion de palabra del 1 al 10 en donde se sustituira para sacar una palabra del arreglo. 
 
-    string y = letra[a];  //con el numero selecionado se remplaza el valor de (y) y sale
+    string y = letra[a]; //seleccion de la posicion del string y que (y) tenga la palabra, asi retornandola.
 
-    return y;
-} //Seleccion aleatoria de palabras 
+    return y; //retorno de (y) el cual es la palabra seleccionada.
+}
 
-string palabram() {
+string palabralvlmedio() {
     string letra[10] = {"llama", "perro", "carro", "mente", "nieve", "cielo", "mundo", "libro", "marea", "estar"};
+
     int a;
     time_t t;
     t = time(NULL);
