cfe3e193af633587ac8d394ac2418f84223792a6
angelf25 <00100224@gmail.com>
2024-07-08 19:15:11 -0600

    correccion minima de errores, para version final

--- juegov2.cpp
+++ juegov2.cpp
@@ -9,7 +9,8 @@
 using namespace std;
 
 // Función para seleccionar la palabra para el nivel fácil
-string palabralvlfacil() {
+string palabralvlfacil()
+{
     string letra[10] = {"mano", "casa", "flor", "azul", "lobo", "cama", "luna", "relo", "vela", "cima"};
     srand(time(NULL));
     int a = rand() % 10;
@@ -17,7 +18,8 @@ string palabralvlfacil() {
 }
 
 // Función para seleccionar la palabra para el nivel medio
-string palabralvlmedio() {
+string palabralvlmedio()
+{
     string letra[10] = {"llama", "perro", "carro", "mente", "nieve", "cielo", "mundo", "libro", "marea", "estar"};
     srand(time(NULL));
     int a = rand() % 10;
@@ -25,7 +27,8 @@ string palabralvlmedio() {
 }
 
 // Función para seleccionar la palabra para el nivel difícil
-string palabralvldificil() {
+string palabralvldificil()
+{
     string letra[10] = {"helicoptero", "estructura", "evaluacion", "conclucion", "desarrollo", "determinar", "naturaleza", "aprovechar", "establecer"};
     srand(time(NULL));
     int a = rand() % 10;
@@ -33,7 +36,8 @@ string palabralvldificil() {
 }
 
 // Función para seleccionar la palabra para el nivel ultra difícil
-string palabralvlultra() {
+string palabralvlultra()
+{
     string letra[10] = {"responsabilidad", "particularmente", "establecimiento", "correspondiente", "arrepentimiento", "entretenimiento", "experimentacion", "sistematizacion", "fortalecimiento", "definitivamente"};
     srand(time(NULL));
     int a = rand() % 10;
@@ -42,18 +46,23 @@ string palabralvlultra() {
 
 const int MAX_INTENTOS = 6;
 
-void dibujarAhorcado(int intentos) {
+void dibujarAhorcado(int intentos)
+{
     cout << "Ahorcado: ";
-    for (int i = 0; i < intentos; i++) {
+    for (int i = 0; i < intentos; i++)
+    {
         cout << "*";
     }
     cout << endl;
 }
 
-bool adivinarLetra(char letra, const string& palabra, vector<char>& letrasAdivinadas) {
+bool adivinarLetra(char letra, const string &palabra, vector<char> &letrasAdivinadas)
+{
     bool encontrada = false;
-    for (char c : palabra) {
-        if (c == letra) {
+    for (char c : palabra)
+    {
+        if (c == letra)
+        {
             encontrada = true;
             letrasAdivinadas.push_back(letra);
         }
@@ -61,12 +70,14 @@ bool adivinarLetra(char letra, const string& palabra, vector<char>& letrasAdivin
     return encontrada;
 }
 
-void jugar(const string& palabra) {
+void jugar(const string &palabra)
+{
     string palabraAdivinada(palabra.size(), '_');
     vector<char> letrasAdivinadas;
     int intentos = 0;
-    
-    while (intentos < MAX_INTENTOS) {
+
+    while (intentos < MAX_INTENTOS)
+    {
         cout << "Palabra a adivinar: " << palabraAdivinada << endl;
         cout << "Intentos restantes: " << MAX_INTENTOS - intentos << endl;
         cout << "Ingrese una letra: ";
@@ -74,83 +85,93 @@ void jugar(const string& palabra) {
         cin >> letra;
         letra = tolower(letra);
 
-        if (find(letrasAdivinadas.begin(), letrasAdivinadas.end(), letra) != letrasAdivinadas.end()) {
+        if (find(letrasAdivinadas.begin(), letrasAdivinadas.end(), letra) != letrasAdivinadas.end())
+        {
             cout << "Letra ya ingresada anteriormente. Intente con otra." << endl;
             continue;
         }
 
-        if (adivinarLetra(letra, palabra, letrasAdivinadas)) {
+        if (adivinarLetra(letra, palabra, letrasAdivinadas))
+        {
             cout << "¡Letra encontrada!" << endl;
-            for (int i = 0; i < palabra.size(); i++) {
-                if (palabra[i] == letra) {
+            for (int i = 0; i < palabra.size(); i++)
+            {
+                if (palabra[i] == letra)
+                {
                     palabraAdivinada[i] = letra;
                 }
             }
-        } else {
+        }
+        else
+        {
             cout << "Letra no encontrada." << endl;
             intentos++;
             dibujarAhorcado(intentos);
         }
 
-        if (palabraAdivinada == palabra) {
-            cout << "¡Felicidades! Has ganado. La palabra era " << palabra << endl;
+        if (palabraAdivinada == palabra)
+        {
+            cout << "¡Felicidades! Has ganado. La palabra era " << palabra << "BIEN JUGADO" << endl;
             return;
         }
     }
 
-    cout << "¡Lo siento! Has perdido. La palabra era: " << palabra << endl;
+    cout << "¡Lo siento! Has perdido. La palabra era: " << palabra << "Mas suerte para la proxima" << endl;
 }
 
-int main() {
+int main()
+{
     int opc;
     string nickname;
     cout << "Ingresa un nickname: ";
     cin >> nickname;
-    
-    do {
+
+    do
+    {
         cout << "\nBienvenidos al juego del ahorcado " << nickname << ". Selecciona la dificultad. Si necesitas ayuda para comprender el juego, aqui te va una ayudita de que va la cosa" << endl;
-        cout << "El juego funciona de la siguiente manera: tendras un repertorio de palabras, esa palabra se seleccionara de manera aleatoria. Tu labor es ingresar letras y adivinar la palabra. Inicia ingresando letras, tendras 6 oportunidades para advinarl la palabra. Si las fallas las 6 oportunidades, se acaba el juego. Se te ira avisando si acertaste o fallaste, para que al final revelar la palabra. Suerte y espero que disfrutes del juego "<< nickname << endl; 
+        cout << "El juego funciona de la siguiente manera: tendras un repertorio de palabras, esa palabra se seleccionara de manera aleatoria. Tu labor es ingresar letras y adivinar la palabra. Inicia ingresando letras, tendras 6 oportunidades para advinarl la palabra. Si las fallas las 6 oportunidades, se acaba el juego. Se te ira avisando si acertaste o fallaste, para que al final revelar la palabra. Suerte y espero que disfrutes del juego " << nickname << endl;
         cout << "Continuando al juego..." << endl;
-        cout << "\n\t1. Facil \n\t2. Medio \n\t3. Dificil \n\t4. Hardcore \n\t 5. Echa un ojo a los creditos de este juego, gracias por jugar. \n\t 6. Si necesitas ayuda presiona el 6.  \n\tPresiona otro numero para salir" << endl; 
-        cin >> opc; 
-        
+        cout << "\n\t1. Facil \n\t2. Medio \n\t3. Dificil \n\t4. Hardcore \n\t 5. Echa un ojo a los creditos de este juego, gracias por jugar. \n\t 6. Si necesitas ayuda presiona el 6.  \n\tPresiona el 0 para salir" << endl;
+        cin >> opc;
+
         string palabra;
-        switch (opc) {
-            case 1:
-                cout << "Has seleccionado la dificultad facil" << endl;
-                palabra = palabralvlfacil();
-                jugar(palabra);
-                break; 
-            case 2:
-                cout << "Has seleccionado la dificultad media" << endl;
-                palabra = palabralvlmedio();
-                jugar(palabra);
-                break;
-            case 3:
-                cout << "Has seleccionado la dificultad dificil" << endl;
-                palabra = palabralvldificil();
-                jugar(palabra);
-                break;
-            case 4:
-                cout << "Has seleccionado la dificultad hardcore" << endl;
-                palabra = palabralvlultra();
-                jugar(palabra);
-                break; 
-            case 0:
-                cout << "Has seleccionado la opción de salir" << endl;
-                break;
-            case 5:
-            cout << "Los creadores de este juego han sido Cesar Chiquillo(00225424), Rodrigo Escobar(00093924) y Fabrizzio Arevalo(00100224)" << endl; 
-            case 6:
-            cout << "Si se te complica, empieza por usar vocales \n No te apresures, tomate tu tiempo, aqui no hay limite de tiempo, toma el que sea necesario. \n Ingresar varias letras en una misma opcion no te ayudara, solo perderas intentos. \n Las palabras no tienen caracteres especiales, asi que no es necesario ingresar dichos caracteres. \n Las palabras no contienen mayusculas, asi que no es necesario ingresarlas. "<< endl; 
+        switch (opc)
+        {
+        case 1:
+            cout << "Has seleccionado la dificultad facil" << endl;
+            palabra = palabralvlfacil();
+            jugar(palabra);
             break;
-            default:
-                cout << "Opcion no valida" << endl;
-                break;
+        case 2:
+            cout << "Has seleccionado la dificultad media" << endl;
+            palabra = palabralvlmedio();
+            jugar(palabra);
+            break;
+        case 3:
+            cout << "Has seleccionado la dificultad dificil" << endl;
+            palabra = palabralvldificil();
+            jugar(palabra);
+            break;
+        case 4:
+            cout << "Has seleccionado la dificultad hardcore" << endl;
+            palabra = palabralvlultra();
+            jugar(palabra);
+            break;
+        case 0:
+            cout << "Has seleccionado la opción de salir" << endl;
+            break;
+        case 5:
+            cout << "Los creadores de este juego han sido Cesar Chiquillo(00225424), Rodrigo Escobar(00093924) y Fabrizzio Arevalo(00100224)" << endl;
+        case 6:
+            cout << "Si se te complica, empieza por usar vocales \n No te apresures, tomate tu tiempo, aqui no hay limite de tiempo, toma el que sea necesario. \n Ingresar varias letras en una misma opcion no te ayudara, solo perderas intentos. \n Las palabras no tienen caracteres especiales, asi que no es necesario ingresar dichos caracteres. \n Las palabras no contienen mayusculas, asi que no es necesario ingresarlas. " << endl;
+            break;
+        default:
+            cout << "Opcion no valida" << endl;
+            break;
         }
     } while (opc != 0);
-    
+
     cout << "Espero que te haya gustado el juego, " << nickname << ". ¡Pasa feliz dia!" << endl;
-    cout << "Jugador inscrito: " << nickname << endl; 
+    cout << "Jugador inscrito: " << nickname << endl;
     return 0;
 }
